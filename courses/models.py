aa3f68dfe141d3bc33b62adc5e4d36f6146e3273
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-07-04 15:19:34 +0200

    Module model for courses app

--- courses/models.py
+++ courses/models.py
@@ -1,29 +1,58 @@
+"""
+Models for Courses application.
+"""
 from django.db import models
 from django.contrib.auth.models import User
 
 
 class Subject(models.Model):
+    """
+    Subject - high level owner of course and module.
+    """
+
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
-    
+
     class Meta:
-        ordering = ['title']
-    
+        ordering = ["title"]
+
     def __str__(self):
-        return self.title
-    
+        return str(self.title)
+
+
 class Course(models.Model):
-    owner = models.ForeignKey(User, related_name='courses_created',
-                              on_delete=models.CASCADE)
-    subject = models.ForeignKey(Subject, related_name='courses',
-                                on_delete=models.CASCADE)
+    """
+    Section of the subject matter.
+    """
+
+    owner = models.ForeignKey(
+        User, related_name="courses_created", on_delete=models.CASCADE
+        )
+    subject = models.ForeignKey(
+        Subject, related_name="courses", on_delete=models.CASCADE
+        )
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
     overview = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
-    
+
     class Meta:
-        ordering = ['-created']
-    
+        ordering = ["-created"]
+
     def __str__(self):
-        return self.title
+        return str(self.title)
+
+
+class Module(models.Model):
+    """
+    Section/Chapter of the course.
+    """
+
+    course = models.ForeignKey(
+        Course, related_name="modules", on_delete=models.CASCADE
+        )
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return str(self.title)
