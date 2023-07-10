d610cb39277c426ee58d12bd16b1109393be5acd
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-07-10 21:28:11 +0200

    Adding ordering to module and content objects + PostgreSQL setup.

--- courses/models.py
+++ courses/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
 
 
 class Subject(models.Model):
@@ -53,9 +54,13 @@ class Module(models.Model):
     course = models.ForeignKey(Course, related_name="modules", on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
+
+    class Meta:
+        ordering = ['order']
 
     def __str__(self):
-        return str(self.title)
+        return f'{self.order}. {self.title}'
 
 
 class Content(models.Model):
@@ -72,6 +77,10 @@ class Content(models.Model):
                                      )})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey("content_type", "object_id")
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
 
 
 class ItemBase(models.Model):
