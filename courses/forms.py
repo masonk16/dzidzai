ee001c8f66c8921f4afa224ff36fc0c112099fe5
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-08-16 21:10:44 +0200

    CourseModuleUpdateView

--- courses/forms.py
+++ courses/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms.models import inlineformset_factory
-from models import Course, Module
+from .models import Course, Module
 
 ModuleFormSet = inlineformset_factory(Course, Module,
                                       fields=['title', 'description'],
