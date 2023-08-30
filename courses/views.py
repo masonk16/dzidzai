3fe48861a42e1500759e93bca472b32ccc954e65
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-08-30 21:39:32 +0200

    Content ordering view.

--- courses/views.py
+++ courses/views.py
@@ -219,3 +219,14 @@ class ModuleOrderView(CsrfExemptMixin,
             Module.objects.filter(id=id,
                                   course__owner=request.user).update(order=order)
         return self.render_json_response({'saved': 'OK'})
+
+
+class ContentOrderView(CsrfExemptMixin,
+                       JsonRequestResponseMixin,
+                       View):
+
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id,
+                                   module__course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
