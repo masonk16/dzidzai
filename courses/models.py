9348557a7cf4bd71110e1ac2fec8bd252b197b3c
Mason Mapfundematsva <masonmapfunde@gmail.com>
2024-01-11 21:46:08 +0200

    Rendering method for content.

--- courses/models.py
+++ courses/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.template.loader import render_to_string
 from .fields import OrderField
 
 
@@ -102,6 +103,10 @@ class ItemBase(models.Model):
 
     def __str__(self):
         return str(self.title)
+
+    def render(self):
+        return render_to_string(f'courses/content/{self._meta.model_name}.html',
+                                {'item': self})
 
 
 class Text(ItemBase):
