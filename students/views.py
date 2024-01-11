590eb6b62b9dcb6ace2611e8fa2f6890fc28ccbb
Mason Mapfundematsva <masonmapfunde@gmail.com>
2024-01-11 20:48:44 +0200

    Student course list view.

--- students/views.py
+++ students/views.py
@@ -1,9 +1,11 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, FormView
+from django.views.generic.list import ListView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import CourseEnrollForm
+from courses.models import Course
 
 
 class StudentRegistrationView(CreateView):
@@ -32,3 +34,12 @@ class StudentEnrollCourseView(LoginRequiredMixin, FormView):
     def get_success_url(self):
         return reverse_lazy('student_course_detail',
                             args=[self.course.id])
+
+
+class StudentCourseListView(LoginRequiredMixin, ListView):
+    model = Course
+    template_name = 'students/course/list.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
