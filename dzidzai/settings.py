12b217b31c146d7c0386022ea94ab1cb3ea84ac7
Mason Mapfundematsva <masonmapfunde@gmail.com>
2024-01-17 21:24:44 +0200

    Add DRF

--- dzidzai/settings.py
+++ dzidzai/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'embed_video',
     'debug_toolbar',
     'redisboard',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -175,4 +176,10 @@ INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
+# DRF Settings
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
