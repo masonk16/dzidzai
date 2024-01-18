f6077479bb11989d2e163084c3f232321864d919
Mason Mapfundematsva <masonmapfunde@gmail.com>
2024-01-18 20:48:43 +0200

    Course Enrolment API URL pattern

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
          name='subject_list'),
     path('subjects/<pk>/', views.SubjectDetailView.as_view(),
          name='subject_detail'),
+    path('courses/<pk>/enroll/', views.CourseEnrollView.as_view(),
+         name='course_enroll'),
 ]
