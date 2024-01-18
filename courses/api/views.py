3dacafa91002b40787c40c0d81d6f298fdd07d43
Mason Mapfundematsva <masonmapfunde@gmail.com>
2024-01-18 21:40:54 +0200

    Contents action for Course ViewSet.

--- courses/api/views.py
+++ courses/api/views.py
@@ -7,6 +7,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 from courses.models import Subject, Course
 from courses.api.serializers import SubjectSerializer, CourseSerializer
+from courses.api.permissions import IsEnrolled
+from courses.api.serializers import CourseWithContentsSerializer
 
 
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
@@ -21,6 +23,14 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+    @action(detail=True,
+            methods=['get'],
+            serializer_class=CourseWithContentsSerializer,
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
 
 class SubjectListView(generics.ListAPIView):
