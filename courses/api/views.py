0503cc6079f56944270ddee69a562a058ea26c3f
Mason Mapfundematsva <masonmapfunde@gmail.com>
2024-01-18 21:09:59 +0200

    Course ViewSet and router.

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from courses.models import Subject, Course
-from courses.api.serializers import SubjectSerializer
+from courses.api.serializers import SubjectSerializer, CourseSerializer
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
 
 
 class SubjectListView(generics.ListAPIView):
