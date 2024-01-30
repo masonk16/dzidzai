928be60bb015c1b7d7cf56360fe6b39b25bd420a
Mason Mapfundematsva <masonmapfunde@gmail.com>
2024-01-30 20:44:48 +0200

    Update ChatConsumer receive method to send message to channel group.

--- chat/consumers.py
+++ chat/consumers.py
@@ -39,5 +39,11 @@ class ChatConsumer(WebsocketConsumer):
         """Called whenever data is received."""
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        # send message to WebSocket
-        self.send(text_data=json.dumps({'message': message}))
+        # send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
