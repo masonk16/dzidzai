c584fe1abc88957ce67f5d1ac9d9317b822473c9
Mason Mapfundematsva <masonmapfunde@gmail.com>
2024-01-30 21:15:20 +0200

    Add context to chat messages

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,6 +1,7 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
+from django.utils import timezone
 
 
 class ChatConsumer(WebsocketConsumer):
@@ -11,6 +12,7 @@ class ChatConsumer(WebsocketConsumer):
 
     def connect(self):
         """Called when a new connection is received."""
+        self.user = self.scope['user']
         self.id = self.scope['url_route']['kwargs']['course_id']
         self.room_group_name = f'chat_{self.id}'
 
@@ -26,8 +28,6 @@ class ChatConsumer(WebsocketConsumer):
     def disconnect(self, close_code):
         """
         Called when the socket closes.
-        Pass is used because there is no need to implement
-         any action when a client closes the connection.
         """
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
@@ -44,7 +44,9 @@ class ChatConsumer(WebsocketConsumer):
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'user': self.user.username,
+                'datetime': timezone.now().isoformat(),
             }
         )
 
